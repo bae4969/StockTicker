d2a89c42303ae4748d7dc22b948f475281c476fe
bae4969 <bae4969@naver.com>
2026-03-17 00:17:34 +0900

    PRIMARY KEY에 인덱스 추가: execution_id에 대한 BTREE 인덱스 생성

--- api/korea_invest.py
+++ api/korea_invest.py
@@ -601,7 +601,8 @@ class ApiKoreaInvestType:
             execution_non_volume DOUBLE UNSIGNED NOT NULL DEFAULT '0',
             execution_ask_volume DOUBLE UNSIGNED NOT NULL DEFAULT '0',
             execution_bid_volume DOUBLE UNSIGNED NOT NULL DEFAULT '0',
-            PRIMARY KEY (execution_datetime, execution_id) USING BTREE
+            PRIMARY KEY (execution_datetime, execution_id) USING BTREE,
+            INDEX idx_execution_id (execution_id) USING BTREE
             ) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB
             PARTITION BY RANGE (YEAR(execution_datetime)) (
             PARTITION pmax VALUES LESS THAN MAXVALUE)"""
